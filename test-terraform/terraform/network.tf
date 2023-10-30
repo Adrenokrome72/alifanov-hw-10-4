bc8289516fc883137a8c2a89690a15976124be0f
Adrenokrome72 <103280846+Adrenokrome72@users.noreply.github.com>
2023-10-30 11:32:49 +0300

    Update network.tf

--- test-terraform/terraform/network.tf
+++ test-terraform/terraform/network.tf
@@ -250,3 +250,18 @@ resource "yandex_vpc_security_group" "public-load-balancer" {
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+# outputs
+output "external_ip_addres_load_balancer" {
+  value = yandex_alb_load_balancer.lb.listener.0.endpoint.0.address.0.external_ipv4_address
+}
+output "bastion" {
+  value = yandex_compute_instance.bastion.network_interface.0.nat_ip_address
+}
+output "kibana" {
+  value = yandex_compute_instance.kibana.network_interface.0.nat_ip_address
+}
+output "grafana" {
+  value = yandex_compute_instance.grafana.network_interface.0.nat_ip_address
+}
