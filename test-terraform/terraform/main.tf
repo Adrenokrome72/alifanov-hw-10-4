603ca93e6ff40b05e653175ab66ae0406ab7b2a1
Adrenokrome72 <103280846+Adrenokrome72@users.noreply.github.com>
2023-11-07 14:29:05 +0300

    Update main.tf

--- test-terraform/terraform/main.tf
+++ test-terraform/terraform/main.tf
@@ -438,3 +438,6 @@ resource "yandex_alb_load_balancer" "balancer" {
     }
   }
 }
+output "external_ip_address_balancer" {
+   value = yandex_alb_load_balancer.balancer.listener.0.endpoint.0.address.0.external_ipv4_address.0.address
+ }
