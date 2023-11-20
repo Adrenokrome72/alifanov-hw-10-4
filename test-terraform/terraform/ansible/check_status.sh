ba3637694ef4e64ae9320044558fc560f1151b0d
root <root@alifanov.alifanov>
2023-11-20 20:25:24 +0300

    test555

--- test-terraform/terraform/ansible/check_status.sh
+++ test-terraform/terraform/ansible/check_status.sh
@@ -1,10 +1,10 @@
 #!/bin/bash/
 
-#ansible kibana -m shell -a "systemctl status kibana"
-#ansible web -m shell -a "systemctl status nginx nginx-log-exporter filebeat node-exporter"
-#ansible elastic -m shell -a "systemctl status elasticsearch"
-#ansible prometheus -m shell -a "systemctl status prometheus"
-#ansible grafana -m shell -a "systemctl status grafana"
+ansible kibana -m shell -a "systemctl status kibana"
+ansible web -m shell -a "systemctl status nginx nginx-log-exporter filebeat node-exporter"
+ansible elastic -m shell -a "systemctl status elasticsearch"
+ansible prometheus -m shell -a "systemctl status prometheus"
+ansible grafana -m shell -a "systemctl status grafana"
 
 yc compute instance list
 yc vpc network list
