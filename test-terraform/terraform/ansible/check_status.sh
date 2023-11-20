86eb640fb5ede215d69a6c5544db943b78eaed73
root <root@alifanov.alifanov>
2023-11-21 00:34:29 +0300

    final1

--- test-terraform/terraform/ansible/check_status.sh
+++ test-terraform/terraform/ansible/check_status.sh
@@ -6,8 +6,11 @@ ansible elastic -m shell -a "systemctl status elasticsearch"
 ansible prometheus -m shell -a "systemctl status prometheus"
 ansible grafana -m shell -a "systemctl status grafana"
 
+curl -v 158.160.133.68:80
+
 yc compute instance list
 yc vpc network list
 yc vpc subnet list
 yc vpc security-group list
 yc compute snapshot-schedule list
+yc compute snapshot list
