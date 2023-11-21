51cd63bb3de0ca6294050ed5f9cf151128b6446d
root <root@alifanov.alifanov>
2023-11-21 19:28:31 +0300

    finalfinal

--- test-terraform/terraform/ansible/check_status.sh
+++ test-terraform/terraform/ansible/check_status.sh
@@ -1,12 +1,12 @@
 #!/bin/bash/
 
 ansible kibana -m shell -a "systemctl status kibana"
-ansible web -m shell -a "systemctl status nginx nginx-log-exporter filebeat node-exporter"
+ansible web -m shell -a "systemctl status nginx filebeat"
 ansible elastic -m shell -a "systemctl status elasticsearch"
 ansible prometheus -m shell -a "systemctl status prometheus"
-ansible grafana -m shell -a "systemctl status grafana"
+ansible grafana -m shell -a "systemctl status grafana-server"
 
-curl -v 158.160.133.68:80
+curl -v 158.160.133.203:80
 
 yc compute instance list
 yc vpc network list
